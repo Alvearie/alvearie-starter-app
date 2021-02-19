bfc9632f9269e5988c9506fde5046a63e3f0b554
festewart <60472475+festewart@users.noreply.github.com>
2021-02-19 11:06:56 -0600

    Update run-unittests.sh

--- tests/run-unittests.sh
+++ tests/run-unittests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "START ENV VARS"
+env
+echo "END ENV VARS"
+
 cat << EOT >> unittest.xml
 <testsuite name="Mocha Tests" tests="2" failures="0" errors="0" skipped="0" timestamp="Thu, 28 Jun 2018 01:03:46 GMT" time="0.011">
 <testcase classname="Array #indexOf()" name="should return -1 when the value is not present" time="0"/>
